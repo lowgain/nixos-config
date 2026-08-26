ef80b69db741870399aaaacac87f3f0609f740cf
Lowgain <logan.t2020@tutanota.com>
2026-08-25 23:45:10 -0400

    fix typo typo

--- modules/nixos/noctalia-greeter.nix
+++ modules/nixos/noctalia-greeter.nix
@@ -6,14 +6,12 @@
     programs.noctalia-greeter = {
       enable = true;
       settings = {
-        appearance = {
-          theme = "Synced";
-          cursor = {
-            path = "${pkgs.bibata-cursors}/share/icons";
-            theme = "Bibata-Modern-Amber";
-            size = 24;
-          };
+        cursor = {
+          path = "${pkgs.bibata-cursors}/share/icons";
+          theme = "Bibata-Modern-Amber";
+          size = 24;
         };
+        appearance.theme = "Synced";
       };
     };
   };
